318e83385f15f5fb8751128b67c55980c19d1694
efe <mehmetefeabali74@gmail.com>
2025-08-12 22:49:28 +0300

    Revert "adding sitcky contact circle buttons ALL THE PAGES"
    This reverts commit dc785030b23b82f4b6c3339bfe1f6c8e38464f98.

--- main.js
+++ main.js
@@ -222,13 +222,6 @@
      });
 
 
-     fetch('sticky-buttons.html')
-     .then(response => response.text())
-     .then(data => {
-        const el = document.getElementById('sticky-buttons');
-        if (el) el.innerHTML = data;
-     });
-
 
 
 document.addEventListener('DOMContentLoaded', function() {
